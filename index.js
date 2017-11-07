22f14c95efa8398c8af311db3951a4033e0a47b4
sleepy <seoweefeng@gmail.com>
2017-11-07 08:09:52 +0800

    cycling and stop buttons work

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
+function randomizeRestaurants(){
+	  var restaurantArray = ["7e", "fishhead noodle", "shitty chinese food"]
+  	  randomNumber = Math.floor(Math.random()* restaurantArray.length)
+  	  document.getElementById('restaurant').innerHTML = restaurantArray[randomNumber]
+  }
+
+  function cycleRestaurants(){
+  	 x = setInterval(randomizeRestaurants, 300);
+  }
+
 function addRestaurantListener(){
   startBtn = document.getElementById('start-random-button');
-  
+
   // Replace 'functionName' with the name of the function that you have written
-  startBtn.addEventListener("click", functionName)
+  
+  startBtn.addEventListener("click", cycleRestaurants);
 
   getBtn = document.getElementById('get-restaurant-button');
   
   // Replace 'functionName' with the name of the function that you have written
-  getBtn.addEventListener("click", functionName);
+  getBtn.addEventListener("click", clear);
+}
+
+function clear() {
+	clearInterval(x);
 }
 
 // wait for DOM content to load before binding an event
